ae6add1a8c4afd60a0b4aaa797c0f67ada3278a3
Bob Marly <33040669+shoot1101@users.noreply.github.com>
2023-08-15 10:09:29 +0200

    Update fxmanifest.lua
    Add name sound

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,6 +13,7 @@ files {
 	"sfx/**/*.awc"
 }
 
+-- F&F Toyota Supra 2JZ I6 Sound
 data_file "AUDIO_SYNTHDATA" "audioconfig/a80ffeng_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/a80ffeng_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/a80ffeng_sounds.dat"
@@ -22,270 +23,331 @@ data_file 'AUDIO_GAMEDATA' 'audioconfig/apollosv8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/apollosv8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_apollosv8'
 
+--BMW Isetta 298CC 4-Stroke 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/aq11bmw298cc_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq11bmw298cc_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq11bmw298cc'
 
+-- Nissan VQ37VHR Turbo 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/aq14nisvq37vhrt_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq14nisvq37vhrt_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq14nisvq37vhrt'
 
+-- Aston Martin 5.9 V12 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/aston59v12_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aston59v12_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aston59v12'
 
+-- Audi 5 Cylinder Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/audi7a_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/audi7a_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_audi7a'
 
+-- Audi 5 Cylinder Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/audiea855_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/audiea855_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_audiea855'
 
+-- Audi 5 Cylinder Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/audiwx_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/audiwx_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_audiwx'
 
+-- BMW Z4 G29 & Supra A90 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/b58b30_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/b58b30_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_b58b30'
 
+-- 2F2F Nissan Skyline GTR R34 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
 
+-- Dodge Ram Cummins 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/cummins5924v_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/cummins5924v_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cummins5924v'
 
+-- Chevrolet Corvette ZR1 LT5 V8 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/czr1eng_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/czr1eng_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/czr1eng_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_czr1eng'
 
+-- Dodge Challenger HEMI 6.4L V8 
 data_file "AUDIO_SYNTHDATA" "audioconfig/dghmieng_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/dghmieng_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/dghmieng_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_dghmieng"
 
+-- Volkswagen-Audi RS6 EA825 V8 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ea825_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea825_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea825'
 
+-- Volkswagen Golf GTI 7 EA888 DSG I4 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ea888_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea888_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea888'
 
+-- Honda S2000 F20C/F22C VTEC Inline-4 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/f20c_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/f20c_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f20c'
 
+-- Ferrari F40
 data_file 'AUDIO_GAMEDATA' 'audioconfig/f40v8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
 
+-- Ferrari Testarossa F113 V12
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/f113_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/f113_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/f113_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f113'
 
+-- Ferrari 458 F136 V8
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/f136_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/f136_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/f136_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f136'
 
+-- Mazda RX-7 FD3S Rotary
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/fd3sid_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/fd3sid_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/fd3sid_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fd3sid'
 
+-- Ford Focus
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/focus2003_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/focus2003_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/focus2003_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_focus2003'
 
+-- Porsche
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/gt3flat6_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/gt3flat6_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/gt3flat6_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gt3flat6'
 
+-- Porsche 911 GT3RS
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/gt3rstun_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/gt3rstun_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/gt3rstun_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gt3rstun'
 
+-- Honda K20A 2.0 I4 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/k20a_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/k20a_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/k20a_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_k20a'
 
+-- Mercedes-Benz 190 E 
 data_file "AUDIO_SYNTHDATA" "audioconfig/kc34merc190e_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/kc34merc190e_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/kc34merc190e_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_kc34merc190e"
 
+-- BMW S1000RR Akrapovic Evo Line Exhaust 
 data_file "AUDIO_GAMEDATA" "audioconfig/kc74s1000rrakevoline_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/kc74s1000rrakevoline_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_kc74s1000rrakevoline"
 
+-- KTM 1290 Super Duke R 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ktm1290r_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ktm1290r_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ktm1290r'
 
+-- Lamborghini Veneno L539 V12 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lamveneng_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lamveneng_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lamveneng_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lamveneng"
 
+-- Lexus LFA V10 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/lfasound_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/lfasound_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lfasound'
 
+-- BMW M4 S55 I6 Akrapovic 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg18bmwm4_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg18bmwm4_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg18bmwm4_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lg18bmwm4"
 
+-- Ford Focus RS Ecoboost 2.3L I4 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/lg21focusrs_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/lg21focusrs_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg21focusrs_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg21focusrs'
 
+-- Honda S2000 F20C I4 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg56hons2k_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg56hons2k_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg56hons2k_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lg56hons2k"
 
+-- Yamaha Raptor 700cc I1 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lgcyc00qbike700_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lgcyc00qbike700_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lgcyc00qbike700_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lgcyc00qbike700"
 
+-- Custom BMW M5 Sound
 data_file 'AUDIO_GAMEDATA' 'audioconfig/m5cracklemod_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/m5cracklemod_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m5cracklemod'
 
+-- Mercedes AMG C63 M156 6.3L V8 
 data_file "AUDIO_SYNTHDATA" "audioconfig/mbnzc63eng_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/mbnzc63eng_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/mbnzc63eng_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_mbnzc63eng"
 
+-- Mercedes-Benz V8 Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/mercedesm113_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercedesm113_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercedesm113'
 
+-- Mercedes-Benz V8 Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/mercedesm155_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercedesm155_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercedesm155'
 
+-- Mercedes-AMG 63 M177 V8 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/mercm177_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercm177_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercm177'
 
+-- Yamaha MT-09/FZ-09 I3 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/mt09eng_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/mt09eng_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/mt09eng_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mt09eng'
 
+-- Lamborghini Aventador L539 V12
 data_file 'AUDIO_GAMEDATA' 'audioconfig/murciev12_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/murciev12_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_murciev12'
 
+-- Dragster Twin-Charged V8SCT
 data_file 'AUDIO_GAMEDATA' 'audioconfig/musv8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/musv8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_musv8'
 
+-- BMW M2/3/4 (BMW N55/S55) Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/n55b30t0_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/n55b30t0_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_n55b30t0'
 
+-- Nissan GTR R35 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/nisgtr35_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/nisgtr35_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/nisgtr35_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nisgtr35'
 
+-- NFS BMW M3 GTR
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/p60b40_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/p60b40_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/p60b40_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_p60b40'
 
+-- Lamborghini Huracan / Audi R8 5.2 V10
 data_file 'AUDIO_GAMEDATA' 'audioconfig/perfov10_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/perfov10_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_perfov10'
 
+-- Porsche 718 (982) MA2 Flat-4 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/porschema2_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/porschema2_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_porschema2'
 
+-- Ford-Shelby Predator 5.2L 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/predatorv8_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/predatorv8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/predatorv8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_predatorv8'
 
+-- OLD GT-R R35 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/r35sound_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
 
+-- Nissan Skyline R32/R33/R34 RB26DETT I6
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/rb26dett_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
 
+-- BMW E46 M3 S54
 data_file 'AUDIO_GAMEDATA' 'audioconfig/s54b32_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/s54b32_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s54b32'
 
+-- BMW M2/3/4 (BMW N55/S55) Pack
 data_file 'AUDIO_GAMEDATA' 'audioconfig/s55b30_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/s55b30_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s55b30'
 
+-- BMW M5/M8 S63 V8 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
 
+-- BMW M3 E92 S65 V8 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/s65b40_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/s65b40_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s65b40'
 
+-- BMW E60 M5 / E64 M6 S85 V10 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/s85b50_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/s85b50_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s85b50'
 
+-- Suzuki GSX-R 1000 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/suzukigsxr1k_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/suzukigsxr1k_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_suzukigsxr1k'
 
+-- 1969 Ford Mustang
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ta008mustang69_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta008mustang69_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta008mustang69'
 
+-- Ferrari Enzo (F140B) 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ta018ferf140b_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta018ferf140b_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta018ferf140b'
 
+-- Kawasaki Ninja H2R 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ta103ninjah2r_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta103ninjah2r_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta103ninjah2r'
 
+-- Toyota 1JZ I6 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/tjz1eng_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/tjz1eng_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/tjz1eng_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tjz1eng'
 
+-- Lamborghini Urus V8
 data_file 'AUDIO_GAMEDATA' 'audioconfig/urusv8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/urusv8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_urusv8'
 
+-- Bugatti Veyron
 data_file 'AUDIO_GAMEDATA' 'audioconfig/veyronsound_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/veyronsound_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_veyronsound'
 
+-- NFSU2 Nissan 350Z 
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/z33u2_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/z33u2_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/z33u2_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_z33u2'
 
+-- Ford GT Ecoboost 3.5 V6 
 data_file 'AUDIO_GAMEDATA' 'audioconfig/aq46forgtebv6_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq46forgtebv6_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq46forgtebv6'
@@ -294,31 +356,37 @@ data_file 'AUDIO_GAMEDATA' 'audioconfig/aq56honf20c_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq56honf20c_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq56honf20c'
 
+--Mitsubishi Lancer Evolution X 4B11T Rally Group N 
 data_file "AUDIO_SYNTHDATA" "audioconfig/kc61evo10rally_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/kc61evo10rally_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/kc61evo10rally_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_kc61evo10rally"
 
+-- BMW M3 E36
 data_file "AUDIO_SYNTHDATA" "audioconfig/kc135bm36_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/kc135bm36_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/kc135bm36_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_kc135bm36"
 
+-- Skoda Fabia R5 WRC I4 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg87skodar5rally_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg87skodar5rally_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg87skodar5rally_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lg87skodar5rally"
 
+-- Cammed Mustang 5.0L V8 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg88camstang_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg88camstang_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg88camstang_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lg88camstang"
 
+-- Mercedes SLS AMG 6.3L V8 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg116slsamg_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg116slsamg_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg116slsamg_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lg116slsamg"
 
+--Lamborghini Murcielago Straight Piped Exhaust V12 
 data_file "AUDIO_SYNTHDATA" "audioconfig/lg123murcisp_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lg123murcisp_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lg123murcisp_sounds.dat"
